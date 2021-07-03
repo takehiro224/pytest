f446a10e4686224f0c08b6c183b25d3c95d25ca7
takehiro224 <mogu.224@gmail.com>
2021-07-03 15:40:01 +0900

    add base5.py

---
+++ base5.py
@@ -0,0 +1,14 @@
+# 2進数、8進数、16進数
+age = 0b111 # 0b
+print(age)
+age = 0o11 # 0o
+print(age)
+age = 0x11 # 0x
+print(age)
+
+# 2進数(バイナリ)へ変換
+print(bin(15)) #0b1111
+# 8進数(オクテット)へ変換
+print(oct(15)) # 0o17
+# 16進数(ヘキサ)へ変換
+print(hex(15)) # 0xf

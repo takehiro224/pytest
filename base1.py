5242bf6a168a98f08794fb69c46e3e4e3b219a73
takehiro224 <mogu.224@gmail.com>
2021-07-03 11:33:08 +0900

    first commit

---
+++ base1.py
@@ -0,0 +1,19 @@
+# 標準入出力、コメント分、変数
+
+print('Hello World') # Hello World
+
+# コメント
+
+"""
+コメント
+コメント
+コメント
+"""
+
+name = input('あなたの名前は何ですか？')
+age = 18
+# format関数
+print('私の名前は = {}, 年齢は{}'.format(name, age))
+
+a = b = 'hello'
+print(a, b)
---
+++ base2.py
@@ -0,0 +1,15 @@
+# 変数の応用
+
+animal = 'dog'
+動物 = 'cat'
+print(動物)
+
+LEAGAL_AGE = 20
+age =18
+if age < LEAGAL_AGE: # ageが20より小さい時に処理を実行
+    print('未成年')
+else:
+    print('成人')
+
+# format文
+print(f'age = {age}')

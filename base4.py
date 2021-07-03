f0385c710668a0b772675cbca55d6b9fa314cada
takehiro224 <mogu.224@gmail.com>
2021-07-03 15:21:08 +0900

    add base4.py

---
+++ base4.py
@@ -0,0 +1,22 @@
+age = 25
+age += 1
+height = 175.5
+print(age + 1)
+print(height + 1)
+value = -1
+
+print(5//2) # 小数点以下切り下げ
+
+# >>, << シフト演算
+# &, | ビット演算
+value = 1
+print(value)
+value = -2
+print(value)
+value = value + 4
+print(value)
+print(value * 4) # 8
+print(value / 3) # 0.66...
+value = 10
+print(value // 3) # 3
+print(value % 3) # 1

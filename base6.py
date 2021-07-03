d773ad3cc2b86909dbee19770a091f331e8a5bb8
takehiro224 <mogu.224@gmail.com>
2021-07-03 16:05:58 +0900

    add base6.py

---
+++ base6.py
@@ -0,0 +1,27 @@
+# 複素数
+a = 1 + 3j
+# 実数1と虚数3の複素数
+b = 3 + 5j
+# 実数3と虚数5の複素数
+
+print(a + b)
+# 複素数4+8jが表示される
+
+print(a * b)
+# 複素数-12+14jが表示される
+
+# complex関数: 複素数を表す方法
+# 実数が3、虚数が2の場合、複素数としてcomplex(3,2)と表示する
+a = complex(1,3)
+b = complex(3, 5)
+print(a + b) # 4+8j
+
+
+print(a,b)
+print(a+b)
+print(a-b)
+print(a*b)
+# 複素数から実数を取り出す real
+
+
+# 複素数から虚数を取り出す imag

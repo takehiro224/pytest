2795e355265a31d40cb072e5685c31d2374cf50d
takehiro224 <mogu.224@gmail.com>
2021-07-11 11:01:08 +0900

    update base8.py

--- base8.py
+++ base8.py
@@ -11,4 +11,12 @@ print(type(str_num)) # <class 'str'>
 print('num = ' + str(int_num)) # num = 12
 float_num = -20.5
 str_float = str(float_num)
-print(str_float)
+print(str_float) # -20.5
+print(type(str_float)) # <class 'str'>
+
+# 文字列からint, float
+msg = '12'
+int_msg = int(msg)
+float_msg = float(msg)
+print('value = {}, type = {}'.format(int_msg, type(int_msg))) # value = 12, type = <class 'int'>
+print('value = {}, type = {}'.format(float_msg, type(float_msg))) # value = 12.0, type = <class 'float'>

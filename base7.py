ed4f7b457906ae80ee8e8df2be1a0b96b5532486
takehiro224 <mogu.224@gmail.com>
2021-07-11 10:57:29 +0900

    update base8.py

--- base7.py
+++ base7.py
@@ -35,11 +35,49 @@ str_test = byte_test.decode('utf-8')
 print(byte_test)
 print(str_test)
 
+
+msg = 'ABCDEABC'
+# count
+print(msg.count('ABC'))
+# startswith
+print(msg.startswith('ABCD'))
+print(msg.startswith('ACD'))
+# endswith
+print(msg.endswith('C'))
+# strip(両端), rstrip(右端), lstrip(左端)
+msg = ' ABC '
+print(msg)
+print(msg.strip()) # 両端を削除
+msg = 'ABCDEABC'
+print(msg.strip('C')) # ABCDEAB
+print(msg.strip('CBA')) # DE(ABC, ABCが削除される)
+print(msg.lstrip('CBA'))
+print(msg.rstrip('CBA'))
+# upper, lower, swapcase, replace, capitalize
+msg = 'abcABC'
+msg_u = msg.upper() # 全てが大文字
+msg_l = msg.lower() # 全てが小文字
+msg_s = msg.swapcase() # 大文字と小文字を入れ替え
+print(msg_u, msg_l, msg_s) # ABCABC abcabc ABCabc
+msg = 'ABCDEABC'
+msg_r = msg.replace('ABC', 'FFF')
+print(msg_r) # FFFDEFFF
+msg_r = msg.replace('ABC', 'FFF', 1)
+print(msg_r) # FFFDEABC
+msg_r = msg.replace('ABC', 'FFF', -1)
+print(msg_r) # FFFDEFFF
+msg = 'hello world'
+print(msg.capitalize()) # Hello world 一番最初の文字が大文字になる
+msg = 'hello World'
+print(msg.capitalize()) # Hello world
+
 # 文字列の一部取り出し、format, islower, isupper
 msg = 'hello, my name is taro'
 print(msg[:5]) # hello
 print(msg[6:]) # my name is taro
 print(msg[1:6]) #
+
+
 
 print('hellow {}'.format('Taro'))
 # Python3.6以上
--- base8.py
+++ base8.py
@@ -1,34 +1,14 @@
-msg = 'ABCDEABC'
-# count
-print(msg.count('ABC'))
-# startswith
-print(msg.startswith('ABCD'))
-print(msg.startswith('ACD'))
-# endswith
-print(msg.endswith('C'))
-# strip(両端), rstrip(右端), lstrip(左端)
-msg = ' ABC '
-print(msg)
-print(msg.strip()) # 両端を削除
-msg = 'ABCDEABC'
-print(msg.strip('C')) # ABCDEAB
-print(msg.strip('CBA')) # DE(ABC, ABCが削除される)
-print(msg.lstrip('CBA'))
-print(msg.rstrip('CBA'))
-# upper, lower, swapcase, replace, capitalize
-msg = 'abcABC'
-msg_u = msg.upper() # 全てが大文字
-msg_l = msg.lower() # 全てが小文字
-msg_s = msg.swapcase() # 大文字と小文字を入れ替え
-print(msg_u, msg_l, msg_s) # ABCABC abcabc ABCabc
-msg = 'ABCDEABC'
-msg_r = msg.replace('ABC', 'FFF')
-print(msg_r) # FFFDEFFF
-msg_r = msg.replace('ABC', 'FFF', 1)
-print(msg_r) # FFFDEABC
-msg_r = msg.replace('ABC', 'FFF', -1)
-print(msg_r) # FFFDEFFF
-msg = 'hello world'
-print(msg.capitalize()) # Hello world 一番最初の文字が大文字になる
-msg = 'hello World'
-print(msg.capitalize()) # Hello world
+# int str 変換
+int_num = 12
+str_num = str(int_num)
+print(str_num)
+print(type(str_num)) # <class 'str'>
+#print('num = ' + int_num) 
+# Traceback (most recent call last):
+#  File "/Users/takehiro/play/python/base8.py", line 6, in <module>
+#    print('num = ' + int_num)
+#TypeError: can only concatenate str (not "int") to str
+print('num = ' + str(int_num)) # num = 12
+float_num = -20.5
+str_float = str(float_num)
+print(str_float)
